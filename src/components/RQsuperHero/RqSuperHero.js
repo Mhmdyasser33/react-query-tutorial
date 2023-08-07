03e110d30b861ae2833c26fe84a08c8a658ac3a3
Mhmdyasser33 <muhmmed.yasserxi@gmail.com>
2023-08-07 08:57:29 -0700

    create separete folder for concept and content of react-query and implement another things like polling and refetch

--- src/components/RQsuperHero/RqSuperHero.js
+++ src/components/RQsuperHero/RqSuperHero.js
@@ -29,31 +29,50 @@ const RqSuperHero = () => {
     marginBottom: '1rem',
   };
 
+  const btnStyle ={
+    background: '#4CAF50',
+    color: 'white',
+    padding: '10px 20px',
+    border: 'none',
+    borderRadius: '4px',
+    cursor: 'pointer',
+    margin : '20px 0 0 0 '
+  }
   const fetched_data = async () =>{
     return axios.get("http://localhost:4000/heros")
     .then((res) => res.data) ;
   }
-  const {isLoading , data , isError,  error , isFetching}  = useQuery('heros' , fetched_data , {
+  const {isLoading , data , isError,  error , isFetching , refetch}  = useQuery('heros' , fetched_data , {
     // default time is 5000
    /*  cacheTime : 5000 , */
-    staleTime : 30000,
+   /*  staleTime : 3000, */
+  /* refetchOnMount : true , */
+ /*  refetchOnWindowFocus : true */
+   /*    refetchInterval : 3000, */
+     /*refetchIntervalInBackground : 60000,*/
+     // prevent refetching the data
+     enabled : false ,
   }) ;
   console.log('====================================');
   console.log("loading :  " + isLoading , "Fetched : " + isFetching);
   console.log('====================================');
-  if(isLoading){
+  if(isLoading || isFetching){
   return <div style={loadingStyle}>Loading...</div>
   }
   if(isError){
     return <div style={errorStyle}>{error.message}</div>
   }
   return (
+    <>
     <div>
      <h1 style={titleStyle}> Heros </h1>
      {data ? data.map((hero) =>(
        <div  style={heroItemStyle}key={hero.id}> {hero.name}</div>
-     )) : "there is no hero"}
+       )) : "there is no data "}
     </div>
+    {/* handle refetch data but manually  */}
+    <button onClick={() => refetch()} style={btnStyle } disabled={data}>Fetch data </button>
+    </>
   )
 }
 
